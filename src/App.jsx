07fea80b485cde1b926218cd0a52683609cea7f3
SAIKAT <88987186+SaikatGantait@users.noreply.github.com>
2025-04-22 03:36:14 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import RemoteEngine from "./components/RemoteEngine";
 import ThankYouPage from './components/ThankYouPage';
-import './index.css'; // Adjust path based on your structure
+import './index.css'; 
 
 const App = () => {
   return (
